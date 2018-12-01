bb513cc385f944b5f330b0be85e0cbc971ef9613
JulienPlante <julien.plante@ipsa.fr>
2018-12-01 15:55:22 +0300

    Plus propre

--- ExoMars2020/Create_two_Processes/Source.cpp
+++ ExoMars2020/Create_two_Processes/Source.cpp
@@ -7,11 +7,11 @@
 int main(int argc, char** argv)
 {
 #ifdef _DEBUG
-	system(R"cmd("C:\Program Files (x86)\Microsoft Visual Studio\2017\Community\MSBuild\15.0\Bin\MSBuild" ..\Network\Network.vcxproj /t:Rebuild /p:DEFINES=NETWORK_PART=1;TargetName=..\custom_build\mast_part;Configuration=Debug;Platform=x86)cmd");
-	system(R"cmd("C:\Program Files (x86)\Microsoft Visual Studio\2017\Community\MSBuild\15.0\Bin\MSBuild" ..\Network\Network.vcxproj /t:Rebuild /p:DEFINES=NETWORK_PART=2;TargetName=..\custom_build\rover_part;Configuration=Debug;Platform=x86)cmd");
+	system(R"cmd("C:\Program Files (x86)\Microsoft Visual Studio\2017\Community\MSBuild\15.0\Bin\MSBuild" ..\Network\Network.vcxproj /t:Rebuild /p:DEFINES=NETWORK_PART=1;IntDir=custom_build_int\;OutDir=custom_build\;TargetName=mast_part;Configuration=Debug;Platform=x86)cmd");
+	system(R"cmd("C:\Program Files (x86)\Microsoft Visual Studio\2017\Community\MSBuild\15.0\Bin\MSBuild" ..\Network\Network.vcxproj /t:Rebuild /p:DEFINES=NETWORK_PART=2;IntDir=custom_build_int\;OutDir=custom_build\;TargetName=rover_part;Configuration=Debug;Platform=x86)cmd");
 #else
-	system(R"cmd("C:\Program Files (x86)\Microsoft Visual Studio\2017\Community\MSBuild\15.0\Bin\MSBuild" ..\Network\Network.vcxproj /t:Rebuild /p:DEFINES=NETWORK_PART=1;TargetName=..\custom_build\mast_part;Configuration=Release;Platform=x86)cmd");
-	system(R"cmd("C:\Program Files (x86)\Microsoft Visual Studio\2017\Community\MSBuild\15.0\Bin\MSBuild" ..\Network\Network.vcxproj /t:Rebuild /p:DEFINES=NETWORK_PART=2;TargetName=..\custom_build\rover_part;Configuration=Release;Platform=x86)cmd");
+	system(R"cmd("C:\Program Files (x86)\Microsoft Visual Studio\2017\Community\MSBuild\15.0\Bin\MSBuild" ..\Network\Network.vcxproj /t:Rebuild /p:DEFINES=NETWORK_PART=1;IntDir=custom_build_int\;OutDir=custom_build\;TargetName=mast_part;Configuration=Release;Platform=x86)cmd");
+	system(R"cmd("C:\Program Files (x86)\Microsoft Visual Studio\2017\Community\MSBuild\15.0\Bin\MSBuild" ..\Network\Network.vcxproj /t:Rebuild /p:DEFINES=NETWORK_PART=2;IntDir=custom_build_int\;OutDir=custom_build\;TargetName=rover_part;Configuration=Release;Platform=x86)cmd");
 #endif
 	
 	unsigned num_cpus = std::thread::hardware_concurrency();
